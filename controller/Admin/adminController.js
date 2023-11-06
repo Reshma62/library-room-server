527a611645053600d66b6741e5de52a3954e079a
ProgramerNila <programernila@gmail.com>
2023-11-06 18:09:50 +0600

    Category added and get route done

--- controller/Admin/adminController.js
+++ controller/Admin/adminController.js
@@ -3,12 +3,26 @@ const { client } = require("../../utils/dbConfige");
 const dataBase = client.db("library-room");
 
 const createCategory = async (req, res) => {
-  const body = req.body;
-  const imgUrl = `/uploads/${req?.file?.filename}`;
-  body.image = imgUrl;
+  try {
+    const body = req.body;
 
-  const user = dataBase.collection("categorys");
-  const result = await user.insertOne(body);
-  res.status(200).send(result);
+    const imgUrl = `/uploads/${req?.file?.filename}`;
+    body.CategoryPhoto = imgUrl;
+    const categorys = dataBase.collection("categorys");
+    const query = { name: body.name };
+    const existingCategory = await categorys.findOne(query);
+    if (existingCategory) {
+      return res.send({ error: "Category already exists. Try Another One" });
+    }
+    const result = await categorys.insertOne(body);
+    res.status(200).send(result);
+  } catch (error) {
+    console.log(error);
+  }
 };
-module.exports = { createCategory };
+const getAllCategory = async (req, res) => {
+  const Categorys = dataBase.collection("categorys");
+  const category = await Categorys.find().toArray();
+  res.send(category);
+};
+module.exports = { createCategory, getAllCategory };
--- routes/api/admin.js
+++ routes/api/admin.js
@@ -2,18 +2,11 @@ const express = require("express");
 const upload = require("../../middleware/multer");
 const _ = express.Router();
 
-const { createCategory } = require("../../controller/Admin/adminController");
-// const multer = require("multer");
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "./uploads");
-//   },
-//   filename: function (req, file, cb) {
-//     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-//     cb(null, file.fieldname + "-" + uniqueSuffix);
-//   },
-// });
+const {
+  createCategory,
+  getAllCategory,
+} = require("../../controller/Admin/adminController");
 
-// const upload = multer({ storage: storage });
 _.post("/create-category", upload.single("image"), createCategory);
+_.get("/getall-category", getAllCategory);
 module.exports = _;
